475cd55c76cd1f171e787722383b871adfb9a755
Rintarooo <310rnomeado@gmail.com>
2021-07-01 14:23:57 +0900

    update

--- Docker/install_docker_20_10.sh
+++ Docker/install_docker_20_10.sh
@@ -14,7 +14,7 @@
 
 # curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - 
 
-#  sudo apt-key fingerprint 0EBFCD88
+ # sudo apt-key fingerprint 0EBFCD88
 	# make sure outputs are 9DC8~
 	# pub   rsa4096 2017-02-22 [SCEA]
 	#       9DC8 5822 9FC7 DD38 854A  E2D8 8D81 803C 0EBF CD88
